c652bec5df93b58a43d340c7520dbb3bc71071f9
Md Rasheduzzaman Rashed <rased.cse.2016@gmail.com>
2022-10-27 14:20:18 +0600

    Readme file edit and react to pdf try but fail

--- src/Components/Pages/Courses/CourseDetails.js
+++ src/Components/Pages/Courses/CourseDetails.js
@@ -82,6 +82,13 @@ const CourseDetails = () => {
             >
               Enroll Now
             </Link>
+            <Link
+              to=''
+              className="rounded-lg bg-blue-700 px-5 mt-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Download PDF
+            </Link>
+            
           </div>
         </div>
       </Card>
